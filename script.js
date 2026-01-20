ce505ff4475fa0893b445460b088a50093755cd9
nehatandon-dev <neha.tandon77@gmail.com>
2026-01-20 22:20:22 +0530

    Add basic form interaction using JavaScript

---
+++ script.js
@@ -0,0 +1,6 @@
+const contactButton = document.querySelector(".contact-form button");
+
+contactButton.addEventListener("click",function(event) {  event.preventDefault();
+         alert("Thank you for contacting!");
+});
+
